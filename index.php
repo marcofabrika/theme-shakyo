dba393aee63db53bad71af0d95be482d51c78121
Mai Kosoba <mmm.tate@gmail.com>
2018-01-30 23:51:34 +0900

    Add 9-23

--- index.php
+++ index.php
@@ -4,6 +4,24 @@
  *
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more spefic matches a query.
- * E.g., it puts together the home page when no home.php file exits.
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme-shakyo
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) : ?>
+
+		</main>
+
+	</div>
