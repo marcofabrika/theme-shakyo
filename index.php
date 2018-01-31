76a3e42c40ab7c3dc5bb14ad567c7340948d95ee
Mai Kosoba <mmm.tate@gmail.com>
2018-01-31 23:51:54 +0900

    Add 24-31

--- index.php
+++ index.php
@@ -21,6 +21,15 @@ get_header(); ?>
 		if ( have_posts() ) :
 
 			if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+
+			<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
 		</main>
 
