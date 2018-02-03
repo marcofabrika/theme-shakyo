88207e7f0661c300d6396a66ec1e8dda2d5b6098
Mai Kosoba <mmm.tate@gmail.com>
2018-02-03 23:54:16 +0900

    Add content.php 6-19

--- template-part/content.php
+++ template-part/content.php
@@ -3,3 +3,18 @@
  * Template part for displaying posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme-shakyo
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+	</header>
