06ef44890e61560dcdab9896ad046e6c7b84dd3d
Mai Kosoba <mmm.tate@gmail.com>
2018-02-04 23:49:16 +0900

    Add content.php 19-27

--- template-part/content.php
+++ template-part/content.php
@@ -17,4 +17,12 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
+
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php
+				theme_shakyo_posted_on();
+				theme_shakyo_posted_by();
+			?>
+		</div><!-- .entry-meta -->
 	</header>
