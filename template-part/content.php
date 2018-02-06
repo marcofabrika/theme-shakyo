a06e7159675696dd06665219590258e833b773a9
Mai Kosoba <mmm.tate@gmail.com>
2018-02-06 23:41:44 +0900

    Finish content.php

--- template-part/content.php
+++ template-part/content.php
@@ -32,5 +32,28 @@
 	<?php theme_shakyo_post_thumbnail(); ?>
 
 	<div class="entry-content">
+		<?php
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'theme-shakyo' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
 
-	</div>
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'theme-shakyo' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php theme_shakyo_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
