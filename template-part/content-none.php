f0d91ab2977416add7a982dfe59f2ff25f0da9c1
Mai Kosoba <mmm.tate@gmail.com>
2018-02-08 23:31:14 +0900

    Add content-none.php 12-37

--- template-part/content-none.php
+++ template-part/content-none.php
@@ -2,9 +2,38 @@
 /**
  * Template part for displaying a message that posts cannot be found
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarcy/
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package theme-shakyo
  */
 
 ?>
+
+<section class="no-results not-found">
+	<header class="page-header">
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'theme-shakyo' ); ?></h1>
+	</header><!-- .page-header -->
+
+	<div class="page-content">
+		<?php
+		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+
+			<p><?php
+				printf(
+					wp_kses(
+						/* translators: 1: link to WP admin new post page. */
+						__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'theme-shakyo' ),
+						array(
+							'a' => array(
+								'href' => array(),
+							),
+						)
+					),
+					esc_url( admin_url( 'post-new.php' ) )
+				);
+			?></p>
+
+		<?php elseif ( is_search() ) : ?>
+	</div>
+
+</section>
