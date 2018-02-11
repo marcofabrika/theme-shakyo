40371fcf059e9f876dca662fa6a6f2eabafebcc9
Mai Kosoba <mmm.tate@gmail.com>
2018-02-11 23:54:36 +0900

    Add content-none.php

--- template-part/content-none.php
+++ template-part/content-none.php
@@ -36,6 +36,15 @@
 		<?php elseif ( is_search() ) : ?>
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'theme-shakyo' ); ?></p>
-	</div>
+			<?php
+				get_search_form();
 
-</section>
+		else : ?>
+
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'theme-shakyo' ); ?></p>
+			<?php
+				get_search_form();
+
+		endif; ?>
+	</div><!-- .page-content -->
+</section><!-- .no-results -->
